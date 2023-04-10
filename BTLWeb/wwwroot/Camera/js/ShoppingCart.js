9f62c57bd3ce052c63c5b93b69b604603586bdd3
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-11 01:09:29 +0700

    update modal

---
+++ BTLWeb/wwwroot/Camera/js/ShoppingCart.js
@@ -0,0 +1,160 @@
+﻿var temp = []
+
+function loadData() {
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/getall',
+        type: 'GET',
+        dataType: 'json',
+        success: function (res) {
+            var str = '';
+            var count = 0;
+            res.forEach(function (item) {
+                temp.push(item)
+                count += 1;
+                var money = item.product.giaLonNhat * item.quantity;
+                str +=
+                    `<tr>
+                        <td class="stt">${count}</td>
+                        <td><img src="../img/ImageCamera/${item.product.anhDaiDien}" alt="" style="width: 50px;"></td>
+                        <td>${item.product.tenSp}</td>
+                        <td class="align-middle price">${item.product.giaLonNhat}</td>
+                        <td class="align-middle">
+                            <div class="input-group quantity mx-auto" style="width: 100px;">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-sm btn-primary btn-minus">
+                                        <i class="fa fa-minus"></i>
+                                    </button>
+                                </div>
+                                <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="${item.quantity}">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-sm btn-primary btn-plus">
+                                        <i class="fa fa-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="align-middle numPrice" id="${count}">${money}</td>
+                    </tr>`
+            })
+
+            if (count == 0) {
+                str = "<tr><td colspan='7'><p>Giỏ hàng trống</p></td></tr>"
+            }
+
+            $('#showCart').html(str);
+
+            $('.quantity button').on('click', function () {
+                var button = $(this);
+                var oldValue = button.parent().parent().find('input').val();
+                if (button.hasClass('btn-plus')) {
+                    var newVal = parseFloat(oldValue) + 1;
+                } else {
+                    if (oldValue > 0) {
+                        var newVal = parseFloat(oldValue) - 1;
+                    } else {
+                        newVal = 0;
+                    }
+                }
+                button.parent().parent().find('input').val(newVal);
+                var parent = $(this).closest("tr");
+                // Tìm đến phần tử có class là "numPrice" trong phần tử cha
+                var numPrice = parent.find(".numPrice");
+                // Thay đổi giá trị của phần tử "numPrice"
+                var price = parent.find(".price").text();
+                var id = parent.find(".stt").text();
+                id = parseInt(id) - 1;
+                temp[id].quantity = newVal;
+                const num = parseFloat(price.replace(',', '.'));
+                numPrice.text(newVal * 1 * num);
+
+                $.ajax({
+                    url: 'https://localhost:44368/shoppingcart/update',
+                    data: {
+                        cartData: JSON.stringify(temp)
+                    },
+                    type: 'POST',
+                    dataType: 'json',
+                    success: function (res) {
+                    }
+                })
+                updateSumMoney()
+            });
+            updateSumMoney() 
+
+        }
+    })
+}
+
+function updateSumMoney() {
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/getall',
+        type: 'GET',
+        dataType: 'json',
+        success: function (res) {
+            var sum = 0
+            res.forEach(function (item) {
+                var money = item.product.giaLonNhat * item.quantity;
+                sum += money;
+            })
+            $('#sumMoney').html(sum)
+            $('#sumAllMoney').html(sum + 10000)
+        }
+    })
+}
+
+loadData();
+
+function btnDeleteAll() {
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/DeleteAll',
+        type: 'POST',
+        dataType: 'json',
+        success: function (res) {
+            if (res.status) {
+                $('#noiDung').html("Xóa thành công!");
+                $('#success_tic').modal('show');
+            } else {
+                alert('Bạn không có sản phẩm nào để xóa!')
+            }
+            loadData();
+        }
+    })
+}
+
+function btnContinue() {
+    window.location.href = "../";
+}
+
+function btnCheckout() {
+
+}
+
+function confirmCheckout() {
+    var hoaDonBan = {
+        TongTienHD: 111,
+        PhuongThucThanhToan: 0,
+        GhiChu: 'cc',
+        TrangThai: 0,
+        MaKhachHang: 'N01'
+    }
+
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/CreateOrder',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            orderViewModel: JSON.stringify(hoaDonBan)
+        },
+        success: function (res) {
+            if (res.status) {
+                $('#noiDung').html("Đơn hàng đã được ghi lại");
+                $('#success_tic').modal('show');
+                btnDeleteAll();
+            } else {
+                alert("Bạn chưa có sản phẩm nào trong giỏ hàng!")
+            }
+        }
+    })
+}
+
+
